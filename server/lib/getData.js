50b4596f708e2b0f1fa837c8af4105fac0d83ba2
dtc105 <dtcooper105@gmail.com>
2024-09-16 21:19:39 -0400

    Integrated firebase, needs clean up
    Co-authored-by: zcote-ric <zcote-ric@users.noreply.github.com>
    Co-authored-by: Doug <ddoherty_6252@email.ric.edu>
    Co-authored-by: natzyflores <natzyflores@users.noreply.github.com>

---
+++ server/lib/getData.js
@@ -0,0 +1,15 @@
+import { db } from "../lib/firebase.js";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+
+
+export async function getAllUsers() {
+    const querySnapshot = await getDocs(query(collection(db, "users")));
+    return querySnapshot.docs.map((doc) => doc.data());
+}
+
+export async function getUserbyId(id) {
+    const querySnapshot = await getDocs(query(collection(db, "users"), where("id", "==", id)));
+    return querySnapshot[0].data();
+}
+
