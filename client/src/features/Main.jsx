ca0eb54cf1266c1102c728a332a5db68077d8fb6
dtc105 <dtcooper105@gmail.com>
2024-09-19 18:14:15 -0400

    setup react page for login, css needed

--- client/src/features/Main.jsx
+++ client/src/features/Main.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Planner from "../pages/Planner.jsx";
+import UserLogin from "../pages/UserLogin.jsx";
 
 
 /**
- * Main body of the
- * @returns component
+ * Main body of the site, not to be confused with /src/main.jsx
+ * @returns {Feature}
  */
 function Main() {    
     return (
-        <main className="bg-400 max-h-mainHeight">
+        <main className="bg-400">
             <Router>
 				<Routes>
 					<Route path="/"         Component={Home} />
 					<Route path="/planner"  Component={Planner} />
+                    <Route path="/login"    Component={UserLogin} />
 				</Routes>
 			</Router>
         </main>
