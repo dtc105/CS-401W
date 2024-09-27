ee5aa9016e15b3e40d8527d05ea68c41ea9a796e
BuildTools <unconfigured@null.spigotmc.org>
2024-09-26 20:45:24 -0400

    cleaned home page, added about and contact tabs to the header, realigned header items, aligned footer to bottom of screen and realigned table and items, enlarged hgroup on home page and added intended text according to prototype

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,10 @@
  */
 function Navbar() {
     
-    return (
-        <nav className="p-4 flex flex-1 justify-start items-center gap-4">
+    return ( <>
+        <nav className="p-4 flex flex-1 justify-start items-end gap-4">
 
-            <a href="/" className="flex justify-center items-center gap-4">
+            <a href="/" className="flex justify-center items-end gap-4">
                 <img src="/assets/logo.png" alt="logo image" className="h-8" />
                 <h1 className="mr-4 text-3xl">PlanIt <span className="text-xl text-zinc-300">Agenda</span></h1>
                 <span>Home</span>
@@ -15,7 +15,8 @@ function Navbar() {
             
             <a href="/planner">Planner</a>
         </nav>
-    );
+
+        </>);
 }
 
 export default Navbar;
