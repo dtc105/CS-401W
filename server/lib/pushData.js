e833f32f1f2b273417c89aff0246f0c58ad5def9
Doug <ddoherty_6252@email.ric.edu>
2024-10-01 17:57:16 -0400

    cleaned up and commented pushData

--- server/lib/pushData.js
+++ server/lib/pushData.js
@@ -3,19 +3,18 @@ import { collection, addDoc, updateDoc, doc, Timestamp} from "firebase/firestore
 
 export async function createDoc(collectionID, data) { // creates a new doc, adds initial data, returns the new docs id
     console.log("4");
-    const date = Timestamp.fromDate(new Date());
+    data = await addTimestamp(data,"dateCreated");
     const res = await addDoc(collection(db, collectionID),data);
-    console.log("in createDoc ",res, );
+    //console.log("in createDoc ",res, );
     return res; //
 }
 
-export async function createEvent(collectionID, data){
+export async function createEvent(collectionID, data){// creates new event framework uses create doc, but also adds an 'lists' collection with an empty doc
     const ref = await createDoc(collectionID, data); // needed await!!!
-    console.log("in createEvent " ,ref);
-    const subCollectionRef = collection(ref, "lists");
-    const ref2= await addDoc(subCollectionRef, {d:"d"});
-    //const ref2 = createDoc(subCollectionRef, {d:"d"});
-    return ref2;
+    const subCollectionRef = collection(ref, "lists"); //collection named lists
+    await addDoc(subCollectionRef, {}); // need to initialze lists collection, creates an empty doc to sit in the new collection
+     //console.log("in createEvent " ,ref);
+    return ref;
 }
 
 export async function changeDoc(collectionID, docID, data) { //pushes updates to a specific doc in a specified collection
@@ -29,7 +28,7 @@ export async function changeDoc(collectionID, docID, data) { //pushes updates to
 //Suggested fieldNames: dateCreated, dateCompleted, dateRead,   
 export async function addTimestamp(data,fieldName) {
     if (typeof fieldName == 'undefined'){fieldName="lastChange";} //if only 'data' is passed (no fieldName) assumes a field of lastChange
-    console.log(fieldName);
+    //console.log(fieldName);
     data[fieldName] = Timestamp.fromDate(new Date());
     return data;
 }
