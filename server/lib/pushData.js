2668ded7511f5bd69b9fc02df49bfb156811b6b8
dtc105 <dtcooper105@gmail.com>
2024-10-02 11:34:07 -0400

    changed comments to be jsdocs, cleaned up some more, added error handling in the case that firestore request fails

--- server/lib/pushData.js
+++ server/lib/pushData.js
@@ -1,34 +1,87 @@
 import { db } from "./firebase.js";
 import { collection, addDoc, updateDoc, doc, Timestamp} from "firebase/firestore";
 
-export async function createDoc(collectionID, data) { // creates a new doc, adds initial data, returns the new docs id
-    console.log("4");
-    data = await addTimestamp(data,"dateCreated");
-    const res = await addDoc(collection(db, collectionID),data);
-    //console.log("in createDoc ",res, );
-    return res; //
-}
-
-export async function createEvent(collectionID, data){// creates new event framework uses create doc, but also adds an 'lists' collection with an empty doc
-    const ref = await createDoc(collectionID, data); // needed await!!!
-    const subCollectionRef = collection(ref, "lists"); //collection named lists
-    await addDoc(subCollectionRef, {}); // need to initialze lists collection, creates an empty doc to sit in the new collection
-     //console.log("in createEvent " ,ref);
+/**
+ * Creates a new doc, adds initial data, returns the new docs ref
+ * @param {string} collectionID 
+ * @param {object} data 
+ * @returns {DocumentReference} A Promise resolved with a DocumentReference pointing to the newly created document after it has been written to the backend (Note that it won't resolve while you're offline).
+ */
+export async function createDoc(collectionID, data) {
+    let ref;
+    try {
+        data = addTimestamp(data,"dateCreated");
+        ref = await addDoc(collection(db, collectionID),data);
+    } catch (e) {
+        console.error(e);
+        return;
+    }
     return ref;
 }
 
-export async function changeDoc(collectionID, docID, data) { //pushes updates to a specific doc in a specified collection
-    data = await addTimestamp(data); //adds/updates a timestamp field to 'data'
-    const res = await updateDoc(doc(db, collectionID, docID),data);
+/**
+ * Creates new event framework uses create doc, but also adds a 'lists' subcollection with an empty doc
+ * @param {string} collectionID 
+ * @param {object} data 
+ * @returns {DocumentReference} A Promise resolved with a DocumentReference pointing to the newly created document after it has been written to the backend (Note that it won't resolve while you're offline).
+ */
+export async function createEvent(collectionID, data){
+    try {
+        const ref = await createDoc(collectionID, data);
+    
+        // Adds a subcollection named "lists"
+        const subCollectionRef = collection(ref, "lists");
+    
+        // Adds an empty doc to the subcollection "lists"
+        await addDoc(subCollectionRef, {});
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+
+    return ref;
+}
+
+/**
+ * Pushes updates to a specific doc in a specified collection
+ * @param {string} collectionID 
+ * @param {string} docID 
+ * @param {object} data 
+ * @returns {string} docId
+ */
+export async function changeDoc(collectionID, docID, data) {
+    try {
+        // adds/updates a timestamp field to 'data'
+        data = addTimestamp(data); 
+    
+        // Sets the specified doc to have data: data
+        await updateDoc(doc(db, collectionID, docID),data);
+    } catch (e) {
+        console.error(e);
+        return;
+    }
     return docID;
 }
 
-//adds/changes a timestamp to show when the doc was last changed  
-//fieldName not required (will default to lastChange if empty)
-//Suggested fieldNames: dateCreated, dateCompleted, dateRead,   
-export async function addTimestamp(data,fieldName) {
-    if (typeof fieldName == 'undefined'){fieldName="lastChange";} //if only 'data' is passed (no fieldName) assumes a field of lastChange
-    //console.log(fieldName);
+/**
+ * Adds/changes a timestamp to show when the doc was last changed  
+ * Valid Field Names: dateCreated, dateCompleted, dateRead, lastChange
+ * @param {object} data 
+ * @param {string | "lastChange"} fieldName 
+ * @returns {object | Error} data with new key/value pair of fieldName/currentDate
+ */ 
+export function addTimestamp(data,fieldName = "lastChange") {
+
+    const validFieldNames = ["dateCreated", "dateCompleted", "dateRead", "lastChange"];
+
+    // If fieldname is invalid throw error
+    if (!validFieldNames.includes(fieldName)) {
+        throw {
+            message: `Invalid fieldname: ${fieldname}`,
+            validFieldNames: validFieldNames
+        }
+    }
+
     data[fieldName] = Timestamp.fromDate(new Date());
     return data;
 }
