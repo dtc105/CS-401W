1d1abcd87b61f07e4d29dc0296a2b0611c8934e6
dtc105 <dtcooper105@gmail.com>
2024-10-07 20:43:38 -0400

    added templates

---
+++ App/src/lib/templates.js
@@ -0,0 +1,40 @@
+export const users = {
+    "id": "",
+    "fName": "",
+    "lName": "",
+    "username": "",
+    "email": "",
+    "organization": "",
+    "title": "",
+    "phone": "",
+    "namePrefix": "",
+    "settings": {
+        "": ""
+    },
+    "privacy": {
+        "": ""
+    },
+    "customLists": []
+};
+
+export const lists = {
+    "checkbox": {
+        "label": "",
+        "value": false
+    },
+    "calendar": {
+        "NOT IMPLEMENTED": "|||||||||||||||||||||||||||||||||||||||||"
+    },
+    "text": {
+        "label": "",
+        "value": ""
+    },
+    "contacts": {
+        "label": "",
+        "value": []
+    },
+    "custom": {
+        "label": "",
+        "value": {}
+    }
+}
